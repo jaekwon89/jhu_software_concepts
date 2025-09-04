11ef493bace34d05337a54b8909cce2c5072d612
jaekwon89 <jkwon30@jh.edu>
2025-09-04 02:18:12 -0700

    Update gradcafe.py
    Changing format to object oriented version

--- module_2/gradcafe.py
+++ module_2/gradcafe.py
@@ -1,15 +1,22 @@
 import urllib3
 from bs4 import BeautifulSoup
 
-base_url = "https://www.thegradcafe.com/"
+class GradCafeScraping:
+    BASE_URL = "https://www.thegradcafe.com/"
 
-http = urllib3.PoolManager()
-response = http.request("GET", base_url +"/survey/")
+    def __init__(self, base_url=BASE_URL):
+        self.base_url = base_url
+        self.http = urllib3.PoolManager()
+
+    def fetch_soup(self, path="/survey/"):
+        response = self.http.request("GET", self.base_url + path)
+        html_text = response.data.decode("utf-8")
+        return BeautifulSoup(html_text, "html.parser")
 
 
-html_text = response.data.decode("utf-8")
 
+if __name__ == '__main__':
+    scraper = GradCafeScraping()
+    soup = scraper.fetch_soup()
 
-soup = BeautifulSoup(html_text, "html.parser")
-
-print(soup.prettify())
+    print(soup.prettify())
