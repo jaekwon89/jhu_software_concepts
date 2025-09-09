f2c5feddb7c1bc47e337375a19e60f2ee3f55159
jaekwon89 <jkwon30@jh.edu>
2025-09-09 02:05:51 -0700

    Update tutorial_practice.py

---
+++ module_3/tutorial_practice.py
@@ -0,0 +1,38 @@
+import psycopg2
+from psycopg2 import OperationalError
+
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+def create_database(connection, query):
+    connection.autocommit = True
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Query executed successfully")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+
+
+def tester():
+    connection = create_connection(
+        "postgres", "postgres", "abc123", "127.0.0.1", "5432"
+    )
+    create_database_query = "CREATE DATABASE sm_app"
+    create_database(connection, create_database_query)
+
+
+if __name__ == '__main__':
+    tester()
