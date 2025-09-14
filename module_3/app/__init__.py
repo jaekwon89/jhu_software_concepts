906677a22e77795beca40cf1583a9f027aeda220
jaekwon89 <jkwon30@jh.edu>
2025-09-14 04:29:32 -0700

    Update __init__.py

--- module_3/app/__init__.py
+++ module_3/app/__init__.py
@@ -3,6 +3,9 @@ from .db import ensure_table
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = "c8338bf3561f5fd6159fd22c546400ab0a302fb4fff02df1"
+    
     ensure_table()
 
     from .routes import bp
