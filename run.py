44a3278e8f20d3219fbbda8e00ebd5ea4d3bbf6b
jaekwon89 <jkwon30@jh.edu>
2025-09-15 01:01:59 -0700

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-# run.py
-from module_1 import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    # The app runs on localhost at port 8080, as required
-    app.run(host="0.0.0.0", port=8080, debug=True)
