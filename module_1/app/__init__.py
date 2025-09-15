136c0fd7839b056242ac3f631396b00946bb74e7
jaekwon89 <jkwon30@jh.edu>
2025-09-15 01:00:46 -0700

    Create __init__.py

---
+++ module_1/app/__init__.py
@@ -0,0 +1,11 @@
+# module_1/app/__init__.py
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__, static_folder="static", template_folder="templates")
+
+    # register blueprints
+    from .pages import pages_bp
+    app.register_blueprint(pages_bp)
+
+    return app
