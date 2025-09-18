71957b79c554f30e88979589bdaf706e95a2cbbf
jaekwon89 <jkwon30@jh.edu>
2025-09-18 14:37:22 -0700

    practice

---
+++ practice/test_subtraction.py
@@ -0,0 +1,4 @@
+from math_functions.subtraction import subtraction
+
+def test_sum():
+    assert subtraction(3,2) == 5
--- practice/test_users.py
+++ practice/test_users.py
@@ -2,6 +2,3 @@
 def test_user_creation(db_conn):
     assert db_conn == "fake_database_connection"
 
-# test_orders.py
-def test_order(db_conn):
-    assert isinstance(db_conn, str)
